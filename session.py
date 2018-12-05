7d5753696ba6b8e6edfdccf969edbd41ee593cee
Clément Martinez <clementmartinezdev@gmail.com>
2018-12-05 18:21:09 +0100

    Really fix Laura's bug this time

--- session.py
+++ session.py
@@ -5,7 +5,7 @@ from settings import DATABASE_FILE
 import dbparser
 import process
 from itertools import islice
-db = parser.open_database(DATABASE_FILE)
-prots = {prot.name: prot.domains for prot in islice(parser.parse(db), 200000)}
+db = dbparser.open_database(DATABASE_FILE)
+prots = {prot.name: prot.domains for prot in islice(dbparser.parse(db), 200000)}
 
 domains = process.extract_domains(prots)
