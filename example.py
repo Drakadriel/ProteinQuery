c62820c16083bf96ffbe3f4e1db7321e12acfce5
Laura Trivino <laura.trivino@telecomnancy.eu>
2018-12-06 12:11:10 +0100

    fusion

--- example.py
+++ example.py
@@ -8,9 +8,86 @@ import process
 from itertools import islice
 
 from bottle import get, run, request, response, static_file
-import neo4j_utils
-from neo4j_utils import *
 
+from neo4j import GraphDatabase
+from settings import DATABASE_FILE
+
+
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "neo"))
+'''
+#Exemples qui marchent
+def add_friend(tx, name, friend_name):
+    tx.run("MERGE (a:Person {name: $name}) "
+           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
+           name=name, friend_name=friend_name)
+
+def print_friends(tx, name):
+    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
+                         "RETURN friend.name ORDER BY friend.name", name=name):
+         print(record["friend.name"])
+
+'''
+# list_domain must be a python list, create a protein with its relatives domains
+def add_prot_graph(tx, protein_name,list_domain):
+    tx.run("MERGE (p:Protein {name: $protein_name})",protein_name=protein_name)
+    for domain in list_domain:
+        tx.run("MERGE (d:Domain {name: $domain_name})",domain_name=domain)
+        tx.run("MATCH (p:Protein{name:$protein_name}),(d:Domain {name:$domain}) MERGE (p)-[:OWN]->(d)",
+                protein_name=protein_name, domain=domain)
+    return ["ok"]
+
+#predicat : les deux proteines sont similaires
+def rel_prot(tx,protein_name_1, protein_name_2, quot):
+    tx.run("MERGE (p:Protein {name: $protein_name})",protein_name=protein_name_1)
+    tx.run("MERGE (p:Protein {name: $protein_name})",protein_name=protein_name_2)
+    tx.run("MATCH (p1:Protein {name: $protein_name_1}),(p2:Protein {name: $protein_name_2}) MERGE (p1)-[:SIMILAR {quotien:$quot}]-(p2)",protein_name_1=protein_name_1, quot=quot, protein_name_2=protein_name_2)
+    return ["ok"]
+
+def match_prot(tx,protein_name):
+    results = tx.run(
+    "MATCH (n:Protein) WHERE n.name=$name RETURN n", name=protein_name
+    )
+        #return json.dumps([{"protein" : row.n} for row in results])
+    return results
+
+def match_prot_all(tx):
+    results = tx.run(
+    "MATCH (n:Protein) RETURN n LIMIT 100"
+    )
+        #return json.dumps([{"protein" : row.n} for row in results])
+    return results
+
+def correct_autoRel(tx):
+    tx.run("MATCH (n)-[rel:SIMILAR]-(r) WHERE n.name=r.name DELETE rel")
+    return ["ok"]
+
+def match_domain(tx):
+        results = tx.run(
+        "MATCH (n:Domain) RETURN n LIMIT 100"
+        )
+        #return json.dumps([{"protein" : row.n} for row in results])
+        return results
+
+def show_similar(tx):
+    results = tx.run("MATCH p=()-[r:SIMILAR]->() RETURN p LIMIT 100")
+    return results
+
+def show_solo_prot(tx):
+    results = tx.run("MATCH (p:Protein) WHERE NOT (p)-[:OWN]-(:Domain) RETURN p LIMIT 100")
+    return results
+
+def show_similar_induce(tx):
+    results = tx.run("MATCH (p1:Protein)-[:OWN]->(d:Domain)<-[:OWN]-(p2:Protein) return p1.name, p2.name, d.name LIMIT 100")
+    return results
+
+
+
+#with driver.session() as session:
+    #session.write_transaction(add_prot_graph)
+    #session.write_transaction(add_friend, "Arthur", "Guinevere")
+    #session.write_transaction(add_friend, "Arthur", "Lancelot")
+    #session.write_transaction(add_friend, "Arthur", "Merlin")
+    #session.read_transaction(print_friends, "Arthur")
 
 @get("/")
 def get_index():
@@ -25,36 +102,47 @@ def get_graph():
 def construct():
     db = dbparser.open_database(DATABASE_FILE)
     for prot in islice(dbparser.parse(db), 200000):
-        neo4j_utils.exec_add(prot.name, prot.domains)
+        session.write_transaction(add_prot_graph,prot.name, prot.domains)
 
 #ne pas reuse car prend une plombe
 @get("/similar")
 def do_similar():
     parser = dbparser.open_proximities("proximities")
-    for prot1,prot2,quot in parser:
-        neo4j_utils.exec_simil(prot1,prot2,quot)
+    with driver.session() as session:
+        for prot1,prot2,quot in parser:
+            session.write_transaction(rel_prot,prot1,prot2, quot)
+        session.write_transaction(correct_autoRel)
 
-    neo4j_utils.exec_delete_auto()
+@get("/show_similar_induce")
+def show_similar_induce():
+    with driver.session() as session:
+        session.read_transaction(show_similar_induce)
 
-@get("/search")
-def get_search():
-    try:
-        q = request.query["q"]
-    except KeyError:
-        return []
-    else:
-
-        return ["search"]
+@get("/show_similar")
+def show_similar():
+    with driver.session() as session:
+        session.read_transaction(show_similar)
 
 @get("/protein")
-def get_protein(name): #replace title with name
-    return neo4j_utils.exec_show_prot()
+def get_protein():
+    with driver.session() as session:
+        session.read_transaction(match_prot_all)
 
-
+@get("/domain")
+def get_domain():
+    with driver.session() as session:
+        session.read_transaction(match_domain)
 
 @get("/soloProt")
 def getSolo():
-    neo4j_utils.exec_show_solo_prot()
+    with driver.session() as session:
+        session.read_transaction(show_solo_prot)
+
+
+##################################"
+##################################""
+
+
 
 if __name__ == "__main__":
     run(port=8080)
